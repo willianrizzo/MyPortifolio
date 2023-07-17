0e77151dbf90145f6391186047ec4d82b8a2fcc1
wiurizzo <willianrizzoratao10@gmail.com>
2023-07-17 14:49:06 -0300

    Nova foto e links rede sociais

--- src/Home/Componentes/Bghome.jsx
+++ src/Home/Componentes/Bghome.jsx
@@ -1,15 +1,15 @@
-import Box from '../../assets/box.svg'
+import Wiu from '../../assets/wiu.png'
 
 function Bghome(){
     return(
-       <div className="flex mt-10">
-            <div className="pl-10 pt-10 w-[45%] flex flex-col gap-2 ml-10">
+       <div className="flex mt-10 ">
+            <div className="pl-10 pt-10 w-[55%] flex flex-col gap-2 ml-10">
                 <p className="text-[#0EE6B7] text-[44px]">Olá</p>
                 <h2 className="text-white font-semibold text-[44px]">Meu nome é Willian,</h2>
                 <h4 className="w-[105%] font-semibold font-outline-2 text-[#fff0] text-[58px]">Sou desenvolvedor <br /> front-end.</h4>
             </div>
             <div>
-                <img className='shadow-2xl' src={Box} alt="" />
+                <img className='w-[50%] ml-[25%] -mt-36' src={Wiu} alt="" />
             </div>
        </div>
     )
