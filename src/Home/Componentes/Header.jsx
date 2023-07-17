1c4104503fef6babc23bb53ac3d4716f29b33911
wiurizzo <willianrizzoratao10@gmail.com>
2023-07-17 14:49:11 -0300

    new

--- src/Home/Componentes/Header.jsx
+++ src/Home/Componentes/Header.jsx
@@ -1,10 +1,10 @@
-import {AiFillLinkedin} from 'react-icons/ai'
-import {AiFillGithub} from 'react-icons/ai'
-import {AiFillInstagram} from 'react-icons/ai'
+import { AiFillLinkedin } from 'react-icons/ai'
+import { AiFillGithub } from 'react-icons/ai'
+import { AiFillInstagram } from 'react-icons/ai'
 import { Link } from 'react-router-dom'
 
-function Header(){
-    return(
+function Header() {
+    return (
         <div className='p-8 pl-20 flex justify-between text-[#dfdede]'>
             <div className="flex gap-10">
                 <button>Experiencias</button>
@@ -12,9 +12,9 @@ function Header(){
                 <button>Contato</button>
             </div>
             <div className="flex gap-6 mr-6">
-                <AiFillGithub className='text-3xl'/> 
-                <AiFillLinkedin className='text-3xl'/>
-                <AiFillInstagram className='text-3xl'/>
+                <a href="https://github.com/willianrizzo"> <AiFillGithub className='text-3xl' /> </a>
+                <a href="https://www.linkedin.com/in/willianrizzo/"> <AiFillLinkedin className='text-3xl' /></a>
+                <a href="https://www.instagram.com/wiurizzo/"> <AiFillInstagram className='text-3xl' /></a>
             </div>
         </div>
     )
